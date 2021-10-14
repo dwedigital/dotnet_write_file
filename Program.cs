2b11df849a05a8d58663f81cada20b91a423d817
dwedigital <dave@dwedigital.com>
2021-10-14 15:37:58 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace console_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Lines = File.ReadAllLines("./file.txt");
+
+            foreach (string line in Lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Enter what you would like to add:");
+
+            string input = Console.ReadLine();
+
+            string[] writeLines = new string[1] { $"\n{input}" };
+
+             Write(writeLines);
+        }
+        public static async void Write(string[] writeLines)
+        {
+            foreach (string line in writeLines)
+            {
+                await File.AppendAllTextAsync("./file.txt", line);
+            }
+
+
+        }
+    }
+}
