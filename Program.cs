0d20ee3970a9636ebe76948dc7d3b316fdd5f007
dwedigital <dave@dwedigital.com>
2021-10-14 15:43:18 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -9,22 +9,30 @@ namespace console_test
     {
         static void Main(string[] args)
         {
+            // create an array of strings from the file that is read
             string[] Lines = File.ReadAllLines("./file.txt");
 
+            // iterate through each line and console it
             foreach (string line in Lines)
             {
                 Console.WriteLine(line);
             }
-            Console.WriteLine("Enter what you would like to add:");
 
+            Console.WriteLine("Enter what you would like to add:");
+            // get a string from user input
             string input = Console.ReadLine();
 
+            // create an array of legnth 1 for the inputted string
+            // added \n to ensure a new line each time
             string[] writeLines = new string[1] { $"\n{input}" };
 
+            //call function Write with the array
              Write(writeLines);
         }
+        //using async due to append all text being asyncrhonous
         public static async void Write(string[] writeLines)
         {
+            // write each line to the file (only ever one in this case)
             foreach (string line in writeLines)
             {
                 await File.AppendAllTextAsync("./file.txt", line);
